7436cc9c21f514baab3bbdb542a960fddccf55dc
Aidy <github@aidanjenkins.co.uk>
2021-04-22 22:35:00 +0100

    Move clientconfig to src

--- src/Config.ts
+++ src/Config.ts
@@ -2,7 +2,7 @@ export module Config {
     let config = null as ClientConfig;
 
     export async function getConfig() {
-        return config ?? (config = await fetch("../clientConfig.json").then(x => x.json() as Promise<ClientConfig>));
+        return config ?? (config = await fetch("clientConfig.json").then(x => x.json() as Promise<ClientConfig>));
     }
 
     export interface ClientConfig {
